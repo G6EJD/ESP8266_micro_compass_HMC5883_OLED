2ce589842b42a051e65a7aeab8d43e7ab047f232
G6EJD <d.l.bird@sky.com>
2017-09-05 17:57:44 +0100

    Update ESP8266_HMC5883_Compass_TN009.ino
    Had to enforce copyright even more

--- ESP8266_HMC5883_Compass_TN009.ino
+++ ESP8266_HMC5883_Compass_TN009.ino
@@ -1,5 +1,16 @@
 //
 // ESP8266 micro sized compass using HMC5883 and displayed on a Wemos (64x48) OLED shield
+/* This source code is protected under the terms of the MIT License and is copyright (c) 2016 by David Bird and permission is hereby granted, free of charge, to
+ * any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, but not to sub-license and/or 
+ * to sell copies of the Software or to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+ *   
+ *   See more at http://dsbird.org.uk 
+ */   
 // (c) D L Bird 2016
 //
 #include <Wire.h>
